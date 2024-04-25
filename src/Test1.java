225ae61ce9848dac7814349d98946b226fbe301d
Nadeem Mohammed <nadeemmd3605@gmail.com>
2024-04-25 15:38:26 -0400

    First Commit...

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		if(hungry == "yes") 
+			System.out.println("he is hungry");
+			else {
+				System.out.println("Not hungry");
+			}
+
+	}
+
+}
